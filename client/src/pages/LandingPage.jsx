c796339d8ef1aed2e788494cd304234e84525136
Tumaini Wekesa <tumainiwamukota@gmail.com>
2025-09-16 22:58:32 +0300

    Üpdate: LoginPage

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function LandingPage(){
+    return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+       This is the Landing Page
+      </h1>
+      
+      <p className="mt-6 text-sm text-center text-gray-600">
+        <Link to="/login" className="text-blue-500 hover:underline">Go to Login Page</Link>
+      </p>
+
+
+    </div>
+  );
+}
+
+export default LandingPage;
