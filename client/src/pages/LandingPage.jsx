57e8b528d9dfa474297d3e62e062a0a5ee50f884
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-09-25 15:54:30 +0300

    fixed content stacks vertically and nicely centered

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,25 +1,28 @@
 import { Link } from "react-router-dom";
 
-function LandingPage(){
+function LandingPage() {
     return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-       This is the Landing Page
-      </h1>
-      
-      <p className="mt-6 text-sm text-center text-gray-600">
-        <Link to="/login" className="text-blue-500 hover:underline">Go to Login Page</Link>
-      </p>
+        <div className="h-screen flex items-center justify-center bg-gray-100">
+            {/* Center everything in a column */}
+            <div className="text-center">
+                <h1 className="text-4xl font-bold text-blue-600">
+                    This is the Landing Page
+                </h1>
 
-        <p className="mt-2 text-sm text-center text-gray-600">
-          
-          <Link to="/signup" className="text-blue-500 hover:underline">
-            Sign Up here
-          </Link>
-        </p>
+                <p className="mt-6 text-sm text-gray-600">
+                    <Link to="/login" className="text-blue-500 hover:underline">
+                        Go to Login Page
+                    </Link>
+                </p>
 
-    </div>
-  );
+                <p className="mt-2 text-sm text-gray-600">
+                    <Link to="/signup" className="text-blue-500 hover:underline">
+                        Sign Up here
+                    </Link>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default LandingPage;
