41cb0bbddc94972dc907bc8a7314e8a9d4df11da
tums2060 <tumainiwamukota@gmail.com>
2025-10-07 00:15:40 +0300

    Update Navbar Landingpage

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Landingpage from './pages/LandingPage.jsx';
 import Login from './pages/Login.jsx';
 import SignUp from './pages/SignUp.jsx';
+import Navbar from './components/Navbar.jsx';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
 
       <Router>
         <Routes>
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import './styles/Navbar.css'; 
+
+function Navbar(){
+
+    return(
+
+        <nav className="navbar">
+            <div className="navbar-left">
+                <a href="/" className="logo">Life Planner</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
