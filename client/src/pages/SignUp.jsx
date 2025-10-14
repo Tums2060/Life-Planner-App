616526556833f3cdfd77ebe00d6d6fecbfa6dd2c
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-10-14 13:30:26 +0300

    Fix

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -36,7 +36,7 @@ function SignUp(){
                         placeholder="Username"
                         value={form.username}
                         onChange={handleChange}
-                        className="w-full py-2 px-4 border rounded-lg focus:ring-2 focus:ring-green-400 focus:outline-none"
+                        className="w-full py-2 px-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-400 focus:outline-none hover:border-green-400 hover:shadow-md hover:shadow-green-100 transition duration-200"
                     />
 
                     <input
