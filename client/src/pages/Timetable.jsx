6dc757351b4763810ca8e73b5a9702cc4e1a2a12
tums2060 <tumainiwamukota@gmail.com>
2025-10-28 00:25:51 +0300

    Input bar for timetable

--- client/src/pages/Timetable.jsx
+++ client/src/pages/Timetable.jsx
@@ -36,6 +36,35 @@ function Timetable(){
         acc[day] = events.filter((e) => e.day === day);
         return acc;
     }, {});
+
+    const typeColors ={
+        Study: "bg-green-100 border-green-300",
+        Class: "bg-purple-100 border-purple-300",
+        Personal: "bg-yellow-100 border-yellow-300",
+    };
+
+
+    return (
+        <div className="p-6 max-w-7xl mx-auto">
+            <h2 className="text-2xl font-bold mb-1">Timetable</h2>
+            <p className="text-gray-500 mb-6">
+                Weekley view with color coded events.
+            </p>
+
+            {/* form */}
+            <div className="flex flex-wrap items-center gap-4 bg-gray-50 p-4 rounded-xl mb-8">
+                <input 
+                type="text"
+                placeholder="e.g. Computer Networks Lab"
+                value={form.title}
+                onChange={(e) => setForm({ ...form, title: e.target.value})}
+                className="border border-gray-300 rounded-lg p-2 flex-1 min-w-[180px]"
+                />
+            </div>
+        </div>
+
+
+    );
 }
 
 export default Timetable;
