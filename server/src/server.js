7f04a01984d70f286a5357c3375a38dceedaba0c
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-10-30 11:12:00 +0300

    mongodb connection with promise return

--- server/src/server.js
+++ server/src/server.js
@@ -19,6 +19,8 @@ const PORT = process.env.PORT || 5001;
 // Routes
 app.use('/api/auth', authRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}/`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
 });
