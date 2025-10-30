913a64314155c1846bb68eeea8c38006a2840ff6
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-10-30 13:15:26 +0300

    correct env load for api url with ternary operator

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.VITE_API_URL
-    ? `${import.meta.env.VITE_API_URL}/api`
+const BASE_URL = process.env.REACT_APP_API_URL
+    ? `${process.env.REACT_APP_API_URL}/api`
     : "http://localhost:5001/api";
 
 const api = axios.create({
