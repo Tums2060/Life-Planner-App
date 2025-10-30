753a90a3c6676073542a1201bdebed79706b55d3
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-10-30 11:05:05 +0300

    integrated auth routes in server

--- server/src/server.js
+++ server/src/server.js
@@ -7,12 +7,17 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
+
 import { connectDB } from './config/db.js';
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}/`);
