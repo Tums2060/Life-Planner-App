22a1778e0cf32fe8696e79ffb64bec1b675682e6
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-10-30 12:47:53 +0300

    axios instance points

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 // in production, there's no localhost so we have to make this dynamic
-const BASE_URL = `${import.meta.env.VITE_API_URL}/api` || 'http://localhost:5001/api';
+const BASE_URL = import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : "http://localhost:5001/api";
 
 const api = axios.create({
     baseURL: BASE_URL,
+    headers: { "Content-Type": "application/json" },
 })
 
 export default api;
