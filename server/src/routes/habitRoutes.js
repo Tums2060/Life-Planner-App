605c973b6cd4cf39270c24cccc9d1f3939d1a4ea
Alex Kigo Mutitu <alexmutitu@strathmore.edu>
2025-11-11 10:54:46 +0300

    added API routes for habit management

---
+++ server/src/routes/habitRoutes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+  createHabit,
+  getHabits,
+  completeHabit,
+  updateHabit,
+  archiveHabit,
+  deleteHabit,
+} from "../controllers/habitController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.route("/")
+  .post(protect, createHabit)
+  .get(protect, getHabits);
+
+router.patch("/:id/complete", protect, completeHabit);
+router.patch("/:id/archive", protect, archiveHabit);
+
+router
+  .route("/:id")
+  .put(protect, updateHabit)
+  .delete(protect, deleteHabit);
+
+export default router;
