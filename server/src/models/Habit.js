af5e44df7219cb45fd8ff07b2c474d6db45325b0
Alex Kigo Mutitu <alexmutitu@strathmore.edu>
2025-11-11 10:51:43 +0300

    Added habit model

---
+++ server/src/models/Habit.js
@@ -0,0 +1,42 @@
+
+import mongoose from "mongoose";
+
+const habitSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: { 
+      type: String, 
+      required: true 
+    },
+    description: { 
+      type: String 
+    },
+    frequency: {
+      type: String,
+      enum: ["daily", "weekly"],
+      default: "daily",
+    },
+    currentStreak: { 
+      type: Number, 
+      default: 0 
+    },
+    longestStreak: { 
+      type: Number, 
+      default: 0 
+    },
+    lastCompleted: { 
+      type: Date 
+    },
+    isArchived: { 
+      type: Boolean, 
+      default: false 
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Habit", habitSchema);
